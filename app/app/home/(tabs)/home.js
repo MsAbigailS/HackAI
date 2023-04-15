df44303b4a8dbb769e0cca7f80e08f58ea9f1ac7
MsAbigailSCS4375 <Abigail.Smith@UTDallas.edu>
2023-04-15 14:46:51 -0500

    creating_homepage

--- app/app/home/(tabs)/home.js
+++ app/app/home/(tabs)/home.js
@@ -1,29 +1,52 @@
 import { useRouter } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import mavericksTeamImage from '../../images/dallas_mavericks_team_logo.png';
 
 export default function Home() {
 
     const router = useRouter();
 
-    const firstName = "John";
-    const lastName = "Doe";
-    const medName = "Ritalin";
-    const medLongName = "Methylphenidate";
-    const days = "Tomorrow";
-    const time = "9:00";
-    const timeSection = "AM"; 
+    const teamNames = [ "Dallas Mavericks",
+                        "Miami Heat"]
 
-    const report = () => router.push("home/drugInfo/info")
-    const prescriptionPage = () => router.push("home/prescriptions")
+    const matchDates = ["7 Apr 2023",
+                        "2 Apr 2023"]
+
+    const matchTimes = ["5:00 PM",
+                        "6:30 PM"]
 
     
 
     return (
-        <View>
-            <Text>
-                fiueroajfioewa
-                </Text>
+        <View style={styles.homepageContainer}>
+            <View style={styles.team_container}>
+                <Image source = {mavericksTeamImage} style={styles.team_logo}></Image>
+                <Text>{teamNames[0]}</Text>
+            </View>
         </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+    homepageContainer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    team_container: {
+      borderWidth: 2,
+      width: "95%",
+      height: "5%",
+      flexDirection: "row"
+    },
+    team_logo: {
+      width: 45,
+      height: 45,
+      borderWidth: 1,
+      borderRadius: 50 
+    },
+   
+  
+  });
