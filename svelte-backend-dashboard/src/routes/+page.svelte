0270632e49bc41a502107a1be121e34823aa2422
Nicholas Zolton <nicholaszolton+git@gmail.com>
2023-04-15 13:15:04 -0500

    Added basic Svelte HelloWorld

--- svelte-backend-dashboard/src/routes/+page.svelte
+++ svelte-backend-dashboard/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+</svelte:head>
+
+<main>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+
+<style>
+
+</style>
